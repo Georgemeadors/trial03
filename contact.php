5cfcf6e84046832348e25c834a0fd9455351e2ce
George Meadors <gmeadors04@gmail.com>
2022-11-16 01:32:31 -0800

    push template pages

---
+++ contact.php
@@ -0,0 +1,10 @@
+<?php 
+	/** 
+	* Template Name: contact
+	* 
+	* @package trial03
+	* 
+	*/ 
+?> 
+<?php get_header(); ?>
+<!-- code goes here --!>
---
+++ homepage.php
@@ -0,0 +1,11 @@
+<?php 
+	/** 
+	* Template Name: home
+	* 
+	* @package trial03
+	* 
+	*/ 
+?> 
+<?php get_header(); ?>
+<!-- code goes here --!>
+<?php get_footer();?>
